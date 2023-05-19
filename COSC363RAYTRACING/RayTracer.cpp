3b1390f322fd5a65c6765269cd970b7b853d7de6
Crook3dTeeth <tom.kirkwood2002@gmail.com>
2023-05-19 14:55:35 +1200

    Syncing changes

--- COSC363RAYTRACING/RayTracer.cpp
+++ COSC363RAYTRACING/RayTracer.cpp
@@ -17,10 +17,13 @@
 #include "TextureBMP.h"
 #include "Plane.h"
 #include <GL/freeglut.h>
+
+#include <thread>
+
 using namespace std;
 
 const float EDIST = 40.0;
-const int NUMDIV = 500;
+const int NUMDIV = 800;
 const int MAX_STEPS = 10;
 const float XMIN = -10.0;
 const float XMAX = 10.0;
@@ -45,11 +48,7 @@ glm::vec3 trace(Ray ray, int step)
     if(ray.index == -1) return backgroundCol;		//no intersection
 	obj = sceneObjects[ray.index];					//object on which the closest point of intersection is found
 
-	if (obj->isTransparent()) {
-
-		
-
-	}
+	color = obj->lighting(lightPos, ray.dir, ray.hit);
 
 
 	if (ray.index == 1)
@@ -101,7 +100,7 @@ glm::vec3 trace(Ray ray, int step)
 	return color;
 }
 
-//---The main display module -----------------------------------------------------------
+//---The main display module ------------------------------------------------------------
 // In a ray tracing application, it just displays the ray traced image by drawing
 // each cell as a quad.
 //---------------------------------------------------------------------------------------
@@ -110,6 +109,10 @@ void display()
 	float xp, yp;  //grid point
 	float cellX = (XMAX - XMIN) / NUMDIV;  //cell width
 	float cellY = (YMAX - YMIN) / NUMDIV;  //cell height
+
+	float cellXSub = cellX / 2.0f;
+	float cellYSub = cellY / 2.0f;
+
 	glm::vec3 eye(0., 0., 0.);
 
 	glClear(GL_COLOR_BUFFER_BIT);
@@ -123,13 +126,122 @@ void display()
 		xp = XMIN + i * cellX;
 		for (int j = 0; j < NUMDIV; j++)
 		{
+
 			yp = YMIN + j * cellY;
 
 			glm::vec3 dir(xp + 0.5 * cellX, yp + 0.5 * cellY, -EDIST);	//direction of the primary ray
 
 			Ray ray = Ray(eye, dir);
 
-			glm::vec3 col = trace(ray, 1); //Trace the primary ray and get the colour value
+			glm::vec3 col(0);
+
+			bool AA = false;
+			if (AA) {
+				float x1 = xp;
+				float y1 = yp;
+
+				float x2 = xp + cellXSub;
+				float y2 = yp;
+
+				float x3 = xp + cellXSub;
+				float y3 = yp + cellYSub;
+
+				float x4 = xp;
+				float y4 = yp + cellYSub;
+
+
+				glm::vec3 col1 = trace(Ray(eye, glm::vec3(x1 + cellXSub, y1 + cellYSub, -EDIST)), 1);
+				glm::vec3 col2 = trace(Ray(eye, glm::vec3(x2 + cellXSub, y2 + cellYSub, -EDIST)), 1);
+				glm::vec3 col3 = trace(Ray(eye, glm::vec3(x3 + cellXSub, y3 + cellYSub, -EDIST)), 1);
+				glm::vec3 col4 = trace(Ray(eye, glm::vec3(x4 + cellXSub, y4 + cellYSub, -EDIST)), 1);
+
+
+				
+
+
+				col = (col1 + col2 + col3 + col4) / 4.0f;
+			} else {
+				col = trace(ray, 1); //Trace the primary ray and get the colour value
+			}
+
+			bool AA2 = false;
+			if (AA2) {
+				int iOffset;
+				int jOffset;
+				if (i > 0) {
+					if (j > 0) {
+						unsigned char leftPixel[4];
+						unsigned char topLeftPixel[4];
+						unsigned char topPixel[4];
+
+						iOffset = i - 1;
+						jOffset = j;
+						xp = XMIN + iOffset * cellX;
+						yp = YMIN + jOffset * cellY;
+						glReadPixels(xp, yp, cellX, cellY, GL_RGBA, GL_UNSIGNED_BYTE, leftPixel);
+						glm::vec3 leftP(leftPixel[0] / 255.0f, leftPixel[1] / 255.0f, leftPixel[2] / 255.0f);
+
+
+						iOffset = i - 1;
+						jOffset = j - 1;
+						xp = XMIN + iOffset * cellX;
+						yp = YMIN + jOffset * cellY;
+						glReadPixels(xp, yp, cellX, cellY, GL_RGBA, GL_UNSIGNED_BYTE, topLeftPixel);
+						glm::vec3 topLeftP(topLeftPixel[0] / 255.0f, topLeftPixel[1] / 255.0f, topLeftPixel[2] / 255.0f);
+
+						iOffset = i;
+						jOffset = j - 1;
+						xp = XMIN + iOffset * cellX;
+						yp = YMIN + jOffset * cellY;
+						glReadPixels(xp, yp, cellX, cellY, GL_RGB, GL_UNSIGNED_BYTE, topPixel);
+						glm::vec3 topP(topPixel[0] / 255.0f, topPixel[1] / 255.0f, topPixel[2] / 255.0f);
+
+						glm::vec3 leftDif = glm::abs(col - leftP);
+						glm::vec3 topLeftDif = glm::abs(col-topLeftP);
+						glm::vec3 topDif = glm::abs(col - topP);
+
+						float avgDifLeft = (leftDif.r + leftDif.g + leftDif.b) / 3.0f;
+						float avgDifTopLeft = (topLeftDif.r + topLeftDif.g + topLeftDif.b) / 3.0f;
+						float avgDifTop = (topDif.r + topDif.g + topDif.b) / 3.0f;		
+
+						float aaThreshhold = 0.5;
+
+						if (avgDifTop > aaThreshhold || avgDifLeft > aaThreshhold || avgDifTopLeft > aaThreshhold) {
+							xp = XMIN + i * cellX;
+							yp = YMIN + j * cellY;
+							float x1 = xp;
+							float y1 = yp;
+
+							float x2 = xp + cellXSub;
+							float y2 = yp;
+
+							float x3 = xp + cellXSub;
+							float y3 = yp + cellYSub;
+
+							float x4 = xp;
+							float y4 = yp + cellYSub;
+
+
+							glm::vec3 col1 = trace(Ray(eye, glm::vec3(x1 + cellXSub, y1 + cellYSub, -EDIST)), 1);
+							glm::vec3 col2 = trace(Ray(eye, glm::vec3(x2 + cellXSub, y2 + cellYSub, -EDIST)), 1); 
+							glm::vec3 col3 = trace(Ray(eye, glm::vec3(x3 + cellXSub, y3 + cellYSub, -EDIST)), 1);
+							glm::vec3 col4 = trace(Ray(eye, glm::vec3(x4 + cellXSub, y4 + cellYSub, -EDIST)), 1);
+
+							col = (col1 + col2 + col3 + col4) / 4.0f;
+						}
+					}
+				}
+
+
+
+			}
+
+			bool AA3 = false;
+			if (AA3) {
+				// Cast ray in corner of pixel to blend, free AA for not much performance hit : https://www.youtube.com/watch?v=A61S_2swwAc
+
+			}
+
 			glColor3f(col.r, col.g, col.b);
 			glVertex2f(xp, yp);				//Draw each cell with its color value
 			glVertex2f(xp + cellX, yp);
@@ -158,12 +270,12 @@ void initialize()
     glClearColor(0, 0, 0, 1);
 
 	
-	Sphere* sphere1 = new Sphere(glm::vec3(0.0, 0.0, -50.0), 2.0);
+	Sphere* sphere1 = new Sphere(glm::vec3(-5, 0.0, -50.0), 2.0);
 
 	sphere1->setColor(glm::vec3(0, 0, 1));   //Set colour to blue
-	//sphere1->setReflectivity(true, 0.8);
-	sphere1->setTransparency(true, 1);
-	sphere1->setRefractivity(true, 1.01, 2);
+	sphere1->setReflectivity(true, 0.8);
+	//sphere1->setTransparency(true, 1);
+	//sphere1->setRefractivity(true, 1.01, 2);
 
 
 	// Half width
@@ -258,7 +370,7 @@ void initialize()
 		glm::vec3(width - mirrorXoffset, -10, backDepth - 1)
 	);
 	backMirror->setColor(glm::vec3(0, 0, 0));
-	backMirror->setReflectivity(true, 1);
+	backMirror->setReflectivity(true, 1);	
 
 	sceneObjects.push_back(sphere1);		 //Add sphere to scene objects
 
@@ -268,8 +380,8 @@ void initialize()
 	sceneObjects.push_back(rightWall);
 	sceneObjects.push_back(topWall);
 	sceneObjects.push_back(backWall);
-	sceneObjects.push_back(frontMirror);
-	sceneObjects.push_back(backMirror);
+	//sceneObjects.push_back(frontMirror);
+	//sceneObjects.push_back(backMirror);
 }
 
 
